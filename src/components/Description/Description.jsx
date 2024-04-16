1c7b890bdc143f2441b4976577d7899e5c1b7244
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-04-16 17:56:08 +0300

    change

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,17 +4,10 @@ export default function Description() {
   return (
     <div className="section">
       <h1 className={css.title}>
-        <span className={css.accentFirst}>Sip</span> Happens Café
+        Sip Happens Café
       </h1>
       <p>
-        Please leave your feedback
-        <span className={css.accentSecond}> about </span>
-        our service
-        <span className={css.accentSecond}> by </span>
-        selecting one
-        <span className={css.accentSecond}> of the </span>
-        options
-        <span className={css.accentSecond}> below</span>.
+        Please leave your feedback about our service by selecting one of the options below.
       </p>
     </div>
   );
